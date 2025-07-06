6a24b885dc2623335ec2a1d3a2fcfe61eac2ac58
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-07-06 12:17:49 +0300

    urls

--- alx_travel_app/urls.py
+++ alx_travel_app/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
+from drf_yasg import openapi
 
 urlpatterns = [
     path('admin/', admin.site.urls),
