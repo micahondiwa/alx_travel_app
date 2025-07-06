32e34e960884469f646d110339b63747b8fccaa3
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-07-06 12:23:08 +0300

    urls

--- listings/urls.py
+++ listings/urls.py
@@ -1,1 +1,7 @@
 from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('swagger/', shema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/', include('listings.urls')),
+]
